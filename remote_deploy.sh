a1e79e1fdc1a3de08671da05d52736fbbec3e447
guanghes <guanghes@gmail.com>
2021-04-23 00:52:19 -0700

    Update remote_deploy.sh

--- remote_deploy.sh
+++ remote_deploy.sh
@@ -4,7 +4,7 @@ newTag=$1
 if [[ $currentDeployment == "green" ]]
 then
   source ./blue
-  ssh opc@$host docker login -u="guanghes" -p="XXXXXXXX" && docker pull guanghes/th3-server:$newTag && docker run -d -p $port:8080 --name blue guanghes/th3-server:$newTag
+  ssh opc@$host "docker login -u=guanghes -p=XXXXXXXX && docker pull guanghes/th3-server:$newTag && docker run -d -p $port:8080 --name blue guanghes/th3-server:$newTag"
   oci lb backend update --load-balancer-id ocid1.loadbalancer.oc1.phx.aaaaaaaarcshxid33k67rmj5bm6eqlkjqrgjdcwrdlfqap6pn3lfw7hsjutq \
     --backend-set-name th3servers --weight 1 --backup false --drain false --backend-name $host:$port --offline false
     
@@ -16,7 +16,7 @@ then
   
 else
   source ./green
-  ssh opc@$host docker login -u="guanghes" -p="XXXXXXXX" && docker pull guanghes/th3-server:$newTag && docker run -d -p $port:8080 --name green guanghes/th3-server:$newTag
+  ssh opc@$host "docker login -u=guanghes -p=XXXXXXXX && docker pull guanghes/th3-server:$newTag && docker run -d -p $port:8080 --name green guanghes/th3-server:$newTag"
   oci lb backend update --load-balancer-id ocid1.loadbalancer.oc1.phx.aaaaaaaarcshxid33k67rmj5bm6eqlkjqrgjdcwrdlfqap6pn3lfw7hsjutq \
     --backend-set-name th3servers --weight 1 --backup false --drain false --backend-name $host:$port --offline false
     
